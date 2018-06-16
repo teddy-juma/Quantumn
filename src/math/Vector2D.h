406271e613ab49d09e151827ef4ebf365004df2a
Humphreys Pinto <humphreyspinto@gmail.com>
2018-06-16 19:04:38 +0300

    2D vec

---
+++ src/math/Vector2D.h
@@ -0,0 +1,48 @@
+#ifndef _VECTOR2D_H
+#define _VECTOR2D_H
+
+#include <cmath>
+template<class T>
+class Vector2D{
+    private:
+        T x;
+        T y;
+
+    public:
+        Vector2D(): x(T(0)), y(T(0)){} //initialize both x and y to 0
+        Vector2D(const T& x, const T& y);
+
+        //getters and setters
+        const T& getX() const;
+        void  setX(const T newX);
+
+        const T& getY() const;
+        void setY(const T newY);
+
+        //simple vector arithmetic
+        const Vector2D& operator +(const Vector2D& other) const; // returns a new vector whose result is
+        // this->x + other.x and this->y + other.y
+        const Vector2D& operator -(const Vector2D& other) const;
+        const Vector2D& operator *(const Vector2D& other) const;
+        const Vector2D& operator /(const Vector2D& other) const;
+
+        const T& operator[] (size_t index) const;
+        T& operator[] (size_t index);
+        /*boolean operation such as if vector->
+        are equal(in the same position)
+        if they are close or far from each other
+        */
+        inline bool operator ==(const Vector2D& other) const;
+        inline bool operator >=(const Vector2D& other) const;
+        inline bool operator <=(const Vector2D& other) const;
+        inline bool operator <(const Vector2D& other) const;
+        inline bool operator >(const Vector2D& other) const;
+        //scalar operations
+        const Vector2D& operator *(float) const;
+        const Vector2D& operator /(float)const ; 
+        //get magnitude from origin(0,0)
+        inline const T getMagnitudeFromOrigin(const Vector2D& other) const;
+ };
+//float getMagnitude(const Vector2D pointA, const Vector2D pointB);
+
+#endif
---
+++ src/tests/tests2D.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../math/Vector2D.cpp"
+
+int main(){
+    Vector2D<float> testVector(5,7);
+    return 0;
+}
